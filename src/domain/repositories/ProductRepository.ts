6f9486c4d8619a058dab343f7ca33f63b4eb1f21
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-25 17:35:09 -0300

    ✨ Create Product entity and ProductRepository interface

---
+++ src/domain/repositories/ProductRepository.ts
@@ -0,0 +1,8 @@
+import Product from '@domain/entities/Product';
+
+export interface ProductRepository {
+  add(product: Product): Promise<Product>;
+  getAll(): Promise<Product[]>;
+  // update(product: Product): Promise<Product>;
+  // delete(id: string): Promise<void>;
+};
